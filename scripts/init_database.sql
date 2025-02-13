10b4bf2b9c64666b0129b2ae338207e961873030
swfawcett <119128614+swfawcett@users.noreply.github.com>
2025-02-13 09:44:22 -0700

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+
+
+-- Create data warehouse database layers schema
+drop schema if exists dw_bronze;
+create schema dw_bronze;
+
+drop schema if exists dw_silver;
+create schema dw_silver;
+
+drop schema if exists dw_gold;
+create schema dw_gold;
